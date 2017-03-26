ecf102bd6d22c8e09634e51385c3cc913334df4f
FearlessSwiftie <fearlessswiftie1322@gmail.com>
2017-03-26 01:02:21 -0700

    entered beta testing

--- main.js
+++ main.js
@@ -11,6 +11,10 @@ const emddb = mysql.createConnection({
 
 const emdbot = new Discord.Client();
 
+var version = "26.03.2017a (beta)";
+
+var draftActive = false;
+
 emdbot.on("ready", () => {
     console.log("Logged in as " + emdbot.user.username + " - " + emdbot.user.id);
 });
@@ -20,16 +24,61 @@ emdbot.on("message", message => {
     var params = command.slice(1, command.length).join(" ");
 
     switch(command[0].toLowerCase()) {
-        case "!test":
-            emddb.query("SELECT * FROM test WHERE value = ?", [command[1]], function(err, rows) {
-                if(rows[0] == null) {
-                    console.log("value does not exist");
+        case "!draftstatus":
+            switch(draftActive) {
+                case true:
+                    message.reply("active.");
+                    break;
+                case false:
+                    message.reply("not active.");
+                    break;
+            }
+            break;
+        case "!startdraft":
+            if(isCaptain(message)) {
+                switch(draftActive) {
+                    case true:
+                        message.reply("there is already an active draft taking place.");
+                        break;
+                    case false:
+                        message.reply("draft started.");
+                        draftActive = true;
+                        break;
+                }
+            } else {
+                message.reply("you don't have permission to start a draft because you aren't a captain.");
+            }
+            break;
+        case "!stopdraft":
+            if(isCaptain(message)) {
+                switch(draftActive) {
+                    case true:
+                        message.reply("draft stopped.");
+                        draftActive = false;
+                        break;
+                    case false:
+                        message.reply("there is no active draft taking place. Start one with `!startdraft`.");
+                }
+            } else {
+                message.reply("you don't have permission to stop drafts because you aren't a captain.");
+            }
+            break; 
+        case "!dbtest": //this is to make sure we are connected to the database
+            emddb.query("SELECT * FROM heroes WHERE hero = ?", [params], function(err, result) {
+                if(result[0] == null) {
+                    console.log("hero does not exist");
+                    message.channel.sendMessage(params + " was not found on the database.");
                 } else {
-                    console.log(rows[0]['value']);
+                    console.log(result[0]["hero"]);
+                    message.channel.sendMessage(result[0]["hero"]);
                 }
             });
             break;
-        case "!eval":
+        case "!version":
+            message.channel.sendMessage("Running version: `" + version + "`.");
+            break;
+//restricted commands
+        case "!eval": //this command is restricted by ID due to its power
             if (message.author.id == config.ownerID) {
                 if (command[1] == null) {
                     message.reply ("You have not specified what you want me to evaluate! :(");
@@ -59,7 +108,7 @@ function clean(text) {
     }
 }
 
-function isCaptain(message) {
+function isCaptain(message) { //we use this function to check if the user is a captain
     return message.member.roles.exists("name", "Captain 1") || message.member.roles.exists("name", "Captain 2");
 }
 
