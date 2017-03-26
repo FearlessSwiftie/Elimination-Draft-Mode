6c92ab2c6d26c331e841917756b82bf0f032eed1
FearlessSwiftie <fearlessswiftie1322@gmail.com>
2017-03-25 20:48:07 -0700

    added strength heroes to the database

--- emd.sql
+++ emd.sql
@@ -4,3 +4,41 @@ CREATE TABLE heroes (
     pattribute VARCHAR(13),
     available BOOLEAN NOT NULL DEFAULT 1
 );
+
+INSERT INTO heroes (hero, pattribute) VALUES ('Earthshaker', 'strength');
+INSERT INTO heroes (hero, pattribute) VALUES ('Sven', 'strength');
+INSERT INTO heroes (hero, pattribute) VALUES ('Tiny', 'strength');
+INSERT INTO heroes (hero, pattribute) VALUES ('Kunkka', 'strength');
+INSERT INTO heroes (hero, pattribute) VALUES ('Beastmaster', 'strength');
+INSERT INTO heroes (hero, pattribute) VALUES ('Dragon Knight', 'strength');
+INSERT INTO heroes (hero, pattribute) VALUES ('Clockwerk', 'strength');
+INSERT INTO heroes (hero, pattribute) VALUES ('Omniknight', 'strength');
+INSERT INTO heroes (hero, pattribute) VALUES ('Huskar', 'strength');
+INSERT INTO heroes (hero, pattribute) VALUES ('Alchemist', 'strength');
+INSERT INTO heroes (hero, pattribute) VALUES ('Brewmaster', 'strength');
+INSERT INTO heroes (hero, pattribute) VALUES ('Treant Protector', 'strength');
+INSERT INTO heroes (hero, pattribute) VALUES ('Io', 'strength');
+INSERT INTO heroes (hero, pattribute) VALUES ('Centaur Warruner', 'strength');
+INSERT INTO heroes (hero, pattribute) VALUES ('Timbersaw', 'strength');
+INSERT INTO heroes (hero, pattribute) VALUES ('Bristleback', 'strength');
+INSERT INTO heroes (hero, pattribute) VALUES ('Tusk', 'strength');
+INSERT INTO heroes (hero, pattribute) VALUES ('Elder Titan', 'strength');
+INSERT INTO heroes (hero, pattribute) VALUES ('Legion Commander', 'strength');
+INSERT INTO heroes (hero, pattribute) VALUES ('Earth Spirit', 'strength');
+INSERT INTO heroes (hero, pattribute) VALUES ('Phoenix', 'strength');
+INSERT INTO heroes (hero, pattribute) VALUES ('Axe', 'strength');
+INSERT INTO heroes (hero, pattribute) VALUES ('Pudge', 'strength');
+INSERT INTO heroes (hero, pattribute) VALUES ('Sand King', 'strength');
+INSERT INTO heroes (hero, pattribute) VALUES ('Slardar', 'strength');
+INSERT INTO heroes (hero, pattribute) VALUES ('Tidehunter', 'strength');
+INSERT INTO heroes (hero, pattribute) VALUES ('Wraith King', 'strength');
+INSERT INTO heroes (hero, pattribute) VALUES ('Lifestealer', 'strength');
+INSERT INTO heroes (hero, pattribute) VALUES ('Night Stalker', 'strength');
+INSERT INTO heroes (hero, pattribute) VALUES ('Doom', 'strength');
+INSERT INTO heroes (hero, pattribute) VALUES ('Spirit Breaker', 'strength');
+INSERT INTO heroes (hero, pattribute) VALUES ('Lycan', 'strength');
+INSERT INTO heroes (hero, pattribute) VALUES ('Chaos Knight', 'strength');
+INSERT INTO heroes (hero, pattribute) VALUES ('Undying', 'strength');
+INSERT INTO heroes (hero, pattribute) VALUES ('Magnus', 'strength');
+INSERT INTO heroes (hero, pattribute) VALUES ('Abaddon', 'strength');
+INSERT INTO heroes (hero, pattribute) VALUES ('Underlord', 'strength');
